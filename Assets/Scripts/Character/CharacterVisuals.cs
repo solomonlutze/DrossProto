2fb050bdafa3f0a25e24e33b70e146f3aa5ea1a2
Sol <solomon.lutze@gmail.com>
2020-06-12 06:35:04 -0400

    bunch of art/bug prefab datas, plus starting bug select screen

--- Assets/Scripts/Character/CharacterVisuals.cs
+++ Assets/Scripts/Character/CharacterVisuals.cs
@@ -4,28 +4,28 @@
 // Visuals are populated from scriptableobjects....... somehow
 public class CharacterVisuals : MonoBehaviour
 {
-    public TraitSlotToCharacterCharacterBodyPartVisualDictionary traitSlotsToVisuals;
+  public TraitSlotToCharacterCharacterBodyPartVisualDictionary traitSlotsToVisuals;
 
-    public void SetCharacterVisuals(TraitSlotToTraitDictionary traits)
+  public void SetCharacterVisuals(TraitSlotToTraitDictionary traits)
+  {
+    foreach (TraitSlot slot in traits.Keys)
     {
-        foreach (TraitSlot slot in traits.Keys)
+      if (!traitSlotsToVisuals.ContainsKey(slot))
+      {
+        Debug.LogError("Need to assign body part transforms to character visuals on character " + gameObject.name);
+      }
+      else
+      {
+        CharacterBodyPartVisual visual = traitSlotsToVisuals[slot];
+        if (visual.visualRenderer1 != null)
         {
-            if (!traitSlotsToVisuals.ContainsKey(slot))
-            {
-                Debug.LogError("Need to assign body part transforms to character visuals on character " + gameObject.name);
-            }
-            else
-            {
-                CharacterBodyPartVisual visual = traitSlotsToVisuals[slot];
-                if (visual.visualRenderer1 != null)
-                {
-                    visual.visualRenderer1.sprite = traits[slot].visual1;
-                }
-                if (visual.visualRenderer2 != null)
-                {
-                    visual.visualRenderer2.sprite = traits[slot].visual2;
-                }
-            }
+          visual.visualRenderer1.sprite = traits[slot].visual1;
         }
+        if (visual.visualRenderer2 != null)
+        {
+          visual.visualRenderer2.sprite = traits[slot].visual2;
+        }
+      }
     }
+  }
 }
