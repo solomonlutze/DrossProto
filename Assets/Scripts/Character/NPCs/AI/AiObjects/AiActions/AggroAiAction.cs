63f7e3cf708e38ab9f94f4d2122cf22c22c21cc2
sol <solomon.lutze@gmail.com>
2021-01-15 14:44:47 -0500

    Dash attacks added
    asdf

--- Assets/Scripts/Character/NPCs/AI/AiObjects/AiActions/AggroAiAction.cs
+++ Assets/Scripts/Character/NPCs/AI/AiObjects/AiActions/AggroAiAction.cs
@@ -4,43 +4,48 @@ using UnityEngine;
 public class AggroAiAction : MoveAiAction
 {
 
-  public float basicAttackWeight = 1.0f;
-  public float blockAttackWeight = 1.0f;
-  public float powerAttackWeight = 1.0f;
-  public float critAttackWeight = 1.0f;
-  public override bool OnEntry(AiStateController controller)
-  {
-    base.OnEntry(controller);
-    float attackRoll = Random.Range(0f, basicAttackWeight + blockAttackWeight + powerAttackWeight + critAttackWeight);
-    // yes I can do this with an array, no I'm not going to
-    if (attackRoll < basicAttackWeight)
+    public float basicAttackWeight = 1.0f;
+    public float blockAttackWeight = 1.0f;
+    public float dashAttackWeight = 1.0f;
+    public float powerAttackWeight = 1.0f;
+    public float critAttackWeight = 1.0f;
+    public override bool OnEntry(AiStateController controller)
     {
-      controller.selectedAttackType = AttackType.Basic;
+        base.OnEntry(controller);
+        float attackRoll = Random.Range(0f, basicAttackWeight + blockAttackWeight + dashAttackWeight + powerAttackWeight + critAttackWeight);
+        // yes I can do this with an array, no I'm not going to
+        if (attackRoll < basicAttackWeight)
+        {
+            controller.selectedAttackType = AttackType.Basic;
+        }
+        else if (attackRoll < basicAttackWeight + blockAttackWeight)
+        {
+            controller.selectedAttackType = AttackType.Blocking;
+        }
+        else if (attackRoll < basicAttackWeight + blockAttackWeight + dashAttackWeight)
+        {
+            controller.selectedAttackType = AttackType.Dash;
+        }
+        else if (attackRoll < basicAttackWeight + blockAttackWeight + dashAttackWeight + powerAttackWeight)
+        {
+            controller.selectedAttackType = AttackType.Charge;
+        }
+        else
+        {
+            controller.selectedAttackType = AttackType.Critical;
+        }
+        Debug.Log("selected attack " + controller.selectedAttackType);
+        return true;
     }
-    else if (attackRoll < basicAttackWeight + blockAttackWeight)
-    {
-      controller.selectedAttackType = AttackType.Blocking;
-    }
-    else if (attackRoll < basicAttackWeight + blockAttackWeight + powerAttackWeight)
-    {
-      controller.selectedAttackType = AttackType.Charge;
-    }
-    else
-    {
-      controller.selectedAttackType = AttackType.Critical;
-    }
-    Debug.Log("selected attack " + controller.selectedAttackType);
-    return true;
-  }
 
-  public override void Act(AiStateController controller)
-  {
-    if (
-      controller.objectOfInterest != null
-      // && Vector2.Distance(controller.objectOfInterest.transform.position, controller.transform.position) < controller.GetAttackRange(controller.characterAttack, controller.attackModifiers)
-      && Vector2.Distance(controller.objectOfInterest.transform.position, controller.transform.position) >= controller.GetAttackRange())
+    public override void Act(AiStateController controller)
     {
-      MoveTowardsObjectOfInterest(controller);
+        if (
+          controller.objectOfInterest != null
+          // && Vector2.Distance(controller.objectOfInterest.transform.position, controller.transform.position) < controller.GetAttackRange(controller.characterAttack, controller.attackModifiers)
+          && Vector2.Distance(controller.objectOfInterest.transform.position, controller.transform.position) >= controller.GetAttackRange())
+        {
+            MoveTowardsObjectOfInterest(controller);
+        }
     }
-  }
 }
---
+++ Assets/Scripts/Character/NPCs/AI/AiObjects/AiDecisions/RecoveringFromDashAiDecision.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu(menuName = "PluggableAi/Decisions/RecoveringFromDash")]
+public class RecoveringFromDashAiDecision : AiDecision
+{
+  public override bool Decide(AiStateController controller)
+  {
+    return controller.IsRecoveringFromDash();
+  }
+}
