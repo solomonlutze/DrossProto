e4b6ce737a59e46ad099b5193bee6f865957688e
sol <solomon.lutze@gmail.com>
2021-01-24 12:27:11 -0500

    legionnaire parts added

--- Assets/Scripts/Character/Player/Items/Traits/Trait.cs
+++ Assets/Scripts/Character/Player/Items/Traits/Trait.cs
@@ -10,15 +10,15 @@ public enum TraitSlot { Head, Thorax, Abdomen, Legs, Wings }
 // P sure this whole class is deprecated
 public enum TraitEffectType
 {
-    Resistance,
-    AnimationInput,
-    CharacterStat,
-    CharacterAttack,
-    CharacterMovementAbility,
-    CharacterPerceptionAbility,
-    Aura,
-    SpawnsObject,
-    CharacterAttackModifiers
+  Resistance,
+  AnimationInput,
+  CharacterStat,
+  CharacterAttack,
+  CharacterMovementAbility,
+  CharacterPerceptionAbility,
+  Aura,
+  SpawnsObject,
+  CharacterAttackModifiers
 }
 
 public enum ConditionallyActivatedTraitCondition { None, NotMoving, Moving, Dashing };
@@ -27,229 +27,229 @@ public enum ConditionallyActivatedTraitCondition { None, NotMoving, Moving, Dash
 [System.Serializable]
 public class TraitEffect
 {
-    public TraitEffectType effectType;
-    public int magnitude;
-    public DamageType damageType;
-    public CharacterStat stat;
-    public CharacterAttackModifiers attackModifiers;
-    // public int[] attackModifiers;
-    public CharacterAttackValue attackValue;
-    public CharacterAttackValue dashAttackValue;
-    public CharacterMovementAbility movementAbility;
-    public CharacterPerceptionAbility perceptionAbility;
-    public Vector2 animationInput;
-    public bool blocksMovement;
-    public GameObject auraPrefab;
-    public ConditionallyActivatedTraitCondition activatingCondition;
-    public float activatingConditionRequiredDuration;
-    public bool triggeredByDash = false;
-    public bool attackCorrodes = false;
-    private string _sourceString = "";
-    public string sourceString
+  public TraitEffectType effectType;
+  public int magnitude;
+  public DamageType damageType;
+  public CharacterStat stat;
+  public CharacterAttackModifiers attackModifiers;
+  // public int[] attackModifiers;
+  public CharacterAttackValue attackValue;
+  public CharacterAttackValue dashAttackValue;
+  public CharacterMovementAbility movementAbility;
+  public CharacterPerceptionAbility perceptionAbility;
+  public Vector2 animationInput;
+  public bool blocksMovement;
+  public GameObject auraPrefab;
+  public ConditionallyActivatedTraitCondition activatingCondition;
+  public float activatingConditionRequiredDuration;
+  public bool triggeredByDash = false;
+  public bool attackCorrodes = false;
+  private string _sourceString = "";
+  public string sourceString
+  {
+    get
     {
-        get
-        {
-            if (_sourceString == "")
-            {
-                _sourceString = Guid.NewGuid().ToString("N").Substring(0, 15);
-            }
-            return _sourceString;
-        }
+      if (_sourceString == "")
+      {
+        _sourceString = Guid.NewGuid().ToString("N").Substring(0, 15);
+      }
+      return _sourceString;
     }
+  }
 
-    public void Apply(Character owner)
+  public void Apply(Character owner)
+  {
+    if (activatingCondition != ConditionallyActivatedTraitCondition.None)
     {
-        if (activatingCondition != ConditionallyActivatedTraitCondition.None)
-        {
-            if (owner.activeConditionallyActivatedTraitEffects.Contains(this))
-            {
-                return;
-            }
-            else
-            {
-                owner.activeConditionallyActivatedTraitEffects.Add(this);
-            }
-        }
-        switch (effectType)
-        {
-            case TraitEffectType.Resistance:
-                owner.damageTypeResistances[damageType] += magnitude;
-                break;
-            case TraitEffectType.AnimationInput:
-                owner.SetAnimationInput(animationInput);
-                if (blocksMovement)
-                {
-                    owner.SetAnimationPreventsMoving(blocksMovement);
-                }
-                break;
-            case TraitEffectType.CharacterMovementAbility:
-                owner.AddMovementAbility(movementAbility);
-                break;
-            case TraitEffectType.CharacterPerceptionAbility:
-                switch (perceptionAbility)
-                {
-                    case CharacterPerceptionAbility.SensitiveAntennae:
-                        GameObject[] scentGameObjects = GameObject.FindGameObjectsWithTag("EnemyScent");
-                        foreach (GameObject obj in scentGameObjects)
-                        {
-                            ParticleSystem ps = obj.GetComponent<ParticleSystem>();
-                            if (ps != null)
-                            {
-                                Debug.Log("setting emission active");
-                                ParticleSystem.EmissionModule em = ps.emission;
-                                em.enabled = true;
-                            }
-                        }
-                        break;
-                }
-                break;
-            case TraitEffectType.CharacterAttackModifiers:
-                if (triggeredByDash)
-                {
-                    owner.ApplyAttackModifier(attackModifiers, true);
-                }
-                else
-                {
-                    owner.ApplyAttackModifier(attackModifiers, false);
-                }
-                break;
-            // case TraitEffectType.CharacterStat:
-            //     owner.AddStatMod(stat, magnitude, sourceString);
-            //     break;
-            case TraitEffectType.Aura:
-                owner.AddAura(this);
-                break;
-        }
+      if (owner.activeConditionallyActivatedTraitEffects.Contains(this))
+      {
+        return;
+      }
+      else
+      {
+        owner.activeConditionallyActivatedTraitEffects.Add(this);
+      }
     }
+    switch (effectType)
+    {
+      case TraitEffectType.Resistance:
+        owner.damageTypeResistances[damageType] += magnitude;
+        break;
+      case TraitEffectType.AnimationInput:
+        owner.SetAnimationInput(animationInput);
+        if (blocksMovement)
+        {
+          owner.SetAnimationPreventsMoving(blocksMovement);
+        }
+        break;
+      case TraitEffectType.CharacterMovementAbility:
+        owner.AddMovementAbility(movementAbility);
+        break;
+      case TraitEffectType.CharacterPerceptionAbility:
+        switch (perceptionAbility)
+        {
+          case CharacterPerceptionAbility.SensitiveAntennae:
+            GameObject[] scentGameObjects = GameObject.FindGameObjectsWithTag("EnemyScent");
+            foreach (GameObject obj in scentGameObjects)
+            {
+              ParticleSystem ps = obj.GetComponent<ParticleSystem>();
+              if (ps != null)
+              {
+                Debug.Log("setting emission active");
+                ParticleSystem.EmissionModule em = ps.emission;
+                em.enabled = true;
+              }
+            }
+            break;
+        }
+        break;
+      case TraitEffectType.CharacterAttackModifiers:
+        if (triggeredByDash)
+        {
+          owner.ApplyAttackModifier(attackModifiers, true);
+        }
+        else
+        {
+          owner.ApplyAttackModifier(attackModifiers, false);
+        }
+        break;
+      // case TraitEffectType.CharacterStat:
+      //     owner.AddStatMod(stat, magnitude, sourceString);
+      //     break;
+      case TraitEffectType.Aura:
+        owner.AddAura(this);
+        break;
+    }
+  }
 
-    public void Expire(Character owner)
+  public void Expire(Character owner)
+  {
+    switch (effectType)
     {
-        switch (effectType)
+      case TraitEffectType.Resistance:
+        owner.damageTypeResistances[damageType] -= magnitude;
+        break;
+      case TraitEffectType.AnimationInput:
+        owner.SetAnimationInput(Vector2.zero);
+        owner.SetAnimationPreventsMoving(false);
+        break;
+      case TraitEffectType.CharacterMovementAbility:
+        owner.RemoveMovementAbility(movementAbility);
+        break;
+      case TraitEffectType.CharacterPerceptionAbility:
+        switch (perceptionAbility)
         {
-            case TraitEffectType.Resistance:
-                owner.damageTypeResistances[damageType] -= magnitude;
-                break;
-            case TraitEffectType.AnimationInput:
-                owner.SetAnimationInput(Vector2.zero);
-                owner.SetAnimationPreventsMoving(false);
-                break;
-            case TraitEffectType.CharacterMovementAbility:
-                owner.RemoveMovementAbility(movementAbility);
-                break;
-            case TraitEffectType.CharacterPerceptionAbility:
-                switch (perceptionAbility)
-                {
-                    case CharacterPerceptionAbility.SensitiveAntennae:
-                        GameObject[] scentGameObjects = GameObject.FindGameObjectsWithTag("EnemyScent");
-                        foreach (GameObject obj in scentGameObjects)
-                        {
-                            ParticleSystem ps = obj.GetComponent<ParticleSystem>();
-                            if (ps != null)
-                            {
-                                ParticleSystem.EmissionModule em = ps.emission;
-                                em.enabled = false;
-                            }
-                        }
-                        break;
-                }
-                break;
-            // case TraitEffectType.CharacterStat:
-            //     owner.RemoveStatMod(sourceString);
-            //     break;
-            case TraitEffectType.Aura:
-                owner.RemoveAura(this);
-                break;
+          case CharacterPerceptionAbility.SensitiveAntennae:
+            GameObject[] scentGameObjects = GameObject.FindGameObjectsWithTag("EnemyScent");
+            foreach (GameObject obj in scentGameObjects)
+            {
+              ParticleSystem ps = obj.GetComponent<ParticleSystem>();
+              if (ps != null)
+              {
+                ParticleSystem.EmissionModule em = ps.emission;
+                em.enabled = false;
+              }
+            }
+            break;
         }
-        if (activatingCondition != ConditionallyActivatedTraitCondition.None
-          && owner.activeConditionallyActivatedTraitEffects.Contains(this)
-        )
-        {
-            owner.activeConditionallyActivatedTraitEffects.Remove(this);
-        }
+        break;
+      // case TraitEffectType.CharacterStat:
+      //     owner.RemoveStatMod(sourceString);
+      //     break;
+      case TraitEffectType.Aura:
+        owner.RemoveAura(this);
+        break;
     }
+    if (activatingCondition != ConditionallyActivatedTraitCondition.None
+      && owner.activeConditionallyActivatedTraitEffects.Contains(this)
+    )
+    {
+      owner.activeConditionallyActivatedTraitEffects.Remove(this);
+    }
+  }
 }
 
 // this is the actual trait
 public class Trait : ScriptableObject
 {
-    public string traitName;
+  public string traitName;
 
-    [TextArea]
-    public string traitDescription;
-    public CharacterAttributeToIntDictionary attributeModifiers;
-    public CharacterSkillData skillData_old; // Not deprecated yet but soon I think?
-    public GameObject owningBugPrefab;
-    public LymphType lymphType;
-    public CharacterSkillData skill; // When this gets used depends on what body part this is equipped to!
-    public WeaponData weaponData;
-    public Sprite visual1;
+  [TextArea]
+  public string traitDescription;
+  public CharacterAttributeToIntDictionary attributeModifiers;
+  public CharacterSkillData skillData_old; // Not deprecated yet but soon I think?
+  public GameObject owningBugPrefab;
+  public LymphType lymphType;
+  public CharacterSkillData skill; // When this gets used depends on what body part this is equipped to!
+  public WeaponData weaponData;
+  public Sprite visual1;
 
-    public Sprite visual2;
+  public Sprite visual2;
 
 #if UNITY_EDITOR
-    // The following is a helper that adds a menu item to create an Trait Asset
-    [MenuItem("Assets/Create/Trait/Trait")]
-    public static void CreateTrait()
+  // The following is a helper that adds a menu item to create an Trait Asset
+  [MenuItem("Assets/Create/Trait/Trait")]
+  public static void CreateTrait()
+  {
+    string path = EditorUtility.SaveFilePanelInProject("Save Trait", "New Trait", "Asset", "Save Trait", "Assets/resources/Data/TraitData/Traits");
+    if (path == "")
+      return;
+    Trait tAsset = ScriptableObject.CreateInstance<Trait>();
+    tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
+    AssetDatabase.CreateAsset(tAsset, path);
+  }
+
+  // The following is a helper that adds a menu item to create an TraitItem Asset
+  [MenuItem("Assets/Create/Trait/TraitSet")]
+  public static void CreateTraitSet()
+  {
+    string path = EditorUtility.SaveFilePanelInProject("Save Trait", "New Trait", "Asset", "Save Trait", "Assets/resources/Data/TraitData/Traits");
+    if (path == "")
+      return;
+    Debug.Log("path: " + path);
+    string[] splitPath = path.Split('/');
+    string objName = splitPath[splitPath.Length - 1];
+    int indexOfPeriod = objName.IndexOf('.');
+    objName = objName.Substring(0, indexOfPeriod);
+    Debug.Log("objName: " + objName);
+    foreach (TraitSlot slot in Enum.GetValues(typeof(TraitSlot)))
     {
-        string path = EditorUtility.SaveFilePanelInProject("Save Trait", "New Trait", "Asset", "Save Trait", "Assets/resources/Data/TraitData/Traits");
-        if (path == "")
-            return;
-        Trait tAsset = ScriptableObject.CreateInstance<Trait>();
-        tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
-        AssetDatabase.CreateAsset(tAsset, path);
+      Trait tAsset = ScriptableObject.CreateInstance<Trait>();
+      tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
+      tAsset.name = objName + "_" + slot.ToString();
+      splitPath[splitPath.Length - 1] = tAsset.name + ".asset";
+      AssetDatabase.CreateAsset(tAsset, String.Join("/", splitPath));
     }
+  }
 
-    // The following is a helper that adds a menu item to create an TraitItem Asset
-    [MenuItem("Assets/Create/Trait/TraitSet")]
-    public static void CreateTraitSet()
-    {
-        string path = EditorUtility.SaveFilePanelInProject("Save Trait", "New Trait", "Asset", "Save Trait", "Assets/resources/Data/TraitData/Traits");
-        if (path == "")
-            return;
-        Debug.Log("path: " + path);
-        string[] splitPath = path.Split('/');
-        string objName = splitPath[splitPath.Length - 1];
-        int indexOfPeriod = objName.IndexOf('.');
-        objName = objName.Substring(0, indexOfPeriod);
-        Debug.Log("objName: " + objName);
-        foreach (TraitSlot slot in Enum.GetValues(typeof(TraitSlot)))
-        {
-            Trait tAsset = ScriptableObject.CreateInstance<Trait>();
-            tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
-            tAsset.name = objName + "_" + slot.ToString();
-            splitPath[splitPath.Length - 1] = tAsset.name + ".asset";
-            AssetDatabase.CreateAsset(tAsset, String.Join("/", splitPath));
-        }
-    }
+  // public static void CreateTraitSetForBug(string bugName)
+  // {
+  //     foreach (TraitSlot slot in Enum.GetValues(typeof(TraitSlot)))
+  //     {
+  //         Trait tAsset = ScriptableObject.CreateInstance<Trait>();
+  //         tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
+  //         tAsset.name = objName + "_" + slot.ToString();
+  //         splitPath[splitPath.Length - 1] = tAsset.name + ".asset";
+  //         AssetDatabase.CreateAsset(tAsset, String.Join("/", splitPath));
+  //     }
+  // }
 
-    // public static void CreateTraitSetForBug(string bugName)
-    // {
-    //     foreach (TraitSlot slot in Enum.GetValues(typeof(TraitSlot)))
-    //     {
-    //         Trait tAsset = ScriptableObject.CreateInstance<Trait>();
-    //         tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
-    //         tAsset.name = objName + "_" + slot.ToString();
-    //         splitPath[splitPath.Length - 1] = tAsset.name + ".asset";
-    //         AssetDatabase.CreateAsset(tAsset, String.Join("/", splitPath));
-    //     }
-    // }
+  public static Trait CreateTraitForBug(Character bug, TraitSlot slot)
+  {
+    Trait tAsset = ScriptableObject.CreateInstance<Trait>();
+    tAsset.name = bug.gameObject.name + "_" + slot.ToString();
+    string completePath = "Assets/resources/Data/TraitData/Traits/" + tAsset.name + ".asset";
+    Debug.Log("complete path: " + completePath);
+    tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
+    tAsset.owningBugPrefab = bug.gameObject;
+    AssetDatabase.CreateAsset(tAsset, completePath);
+    // Select the object in the project folder
+    Selection.activeObject = tAsset;
 
-    public static Trait CreateTraitForBug(Character bug, TraitSlot slot)
-    {
-        Trait tAsset = ScriptableObject.CreateInstance<Trait>();
-        tAsset.name = bug.gameObject.name + "_" + slot.ToString();
-        string completePath = "Assets/resources/Data/TraitData/Traits/" + tAsset.name + ".asset";
-        Debug.Log("complete path: " + completePath);
-        tAsset.attributeModifiers = new CharacterAttributeToIntDictionary(false);
-        tAsset.owningBugPrefab = bug.gameObject;
-        AssetDatabase.CreateAsset(tAsset, completePath);
-        // Select the object in the project folder
-        Selection.activeObject = tAsset;
-
-        // Also flash the folder yellow to highlight it
-        EditorGUIUtility.PingObject(tAsset);
-        return tAsset;
-    }
+    // Also flash the folder yellow to highlight it
+    EditorGUIUtility.PingObject(tAsset);
+    return tAsset;
+  }
 
 #endif
 }
@@ -264,14 +264,14 @@ public class Trait : ScriptableObject
 [System.Serializable]
 public class UpcomingLifeTrait
 {
-    public Trait trait;
-    public InventoryEntry inventoryItem;
-    public LymphType lymphType;
+  public Trait trait;
+  public InventoryEntry inventoryItem;
+  public LymphType lymphType;
 
-    public UpcomingLifeTrait(Trait t, LymphType lt, InventoryEntry ie)
-    {
-        trait = t;
-        lymphType = lt;
-        inventoryItem = ie;
-    }
+  public UpcomingLifeTrait(Trait t, LymphType lt, InventoryEntry ie)
+  {
+    trait = t;
+    lymphType = lt;
+    inventoryItem = ie;
+  }
 }
