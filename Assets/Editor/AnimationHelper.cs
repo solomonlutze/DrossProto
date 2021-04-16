cba4b6f69bfd11c94a07debe4475c3c8485157f8
sol <solomon.lutze@gmail.com>
2021-04-16 15:46:58 -0400

    animation graph builder seems to work :tada:

--- Assets/Editor/AnimationHelper.cs
+++ Assets/Editor/AnimationHelper.cs
@@ -12,28 +12,26 @@ namespace Infrastructure.Editor
     private static void RebuildAnimationData()
     {
       BugSpeciesToAnimationData bugAnimationDatas = Resources.Load("Prefabs/Characters/Animation/BugAnimationData/BugSpeciesToAnimationData") as BugSpeciesToAnimationData;
-      Debug.Log("data is defined: " + bugAnimationDatas);
       AnimatorController animationController = Resources.Load("Prefabs/Characters/Animation/BugAnimationController") as AnimatorController;
       AnimatorControllerLayer baseLayer = System.Array.Find(animationController.layers, l => l.name == "Base");
+      AnimatorControllerLayer[] layers = animationController.layers;
       foreach (ChildAnimatorState state in baseLayer.stateMachine.states)
       {
         AnimationName stateName = (AnimationName)System.Enum.Parse(typeof(AnimationName), state.state.name);
-        foreach (AnimatorControllerLayer layer in animationController.layers)
+        for (int i = 0; i < layers.Length; i++)
         {
-          if (layer.name == "Base") { continue; }
-          // foreach (ChildAnimatorState state in layer.stateMachine.states)
-          // {
-          // Debug.Log("Evaluating state name: " + state.state.name + " for layer " + layer.name);
-          // AnimationState animationState = state.state.name
+          if (layers[i].name == "Base") { continue; }
           BlendTree tree = new BlendTree();
+          tree.blendParameter = layers[i].name + "AnimationType";
+          tree.useAutomaticThresholds = false;
           foreach (BugSpecies bugSpecies in bugAnimationDatas.bugSpeciesToAnimationData.Keys)
-          // for (int i = 0; i < bugAnimationDatas.bugSpeciesToAnimationData.Keys.Count; i++)
           {
             tree.AddChild(bugAnimationDatas.bugSpeciesToAnimationData[bugSpecies].animationStateNamesToClips[stateName], (int)bugSpecies);
           }
-          state.state.motion = tree;
+          layers[i].SetOverrideMotion(state.state, tree);
         }
       }
+      animationController.layers = layers;
     }
 
   }
