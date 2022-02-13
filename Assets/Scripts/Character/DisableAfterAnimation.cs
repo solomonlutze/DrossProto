e23ce516300a147bb954eb0617a2d7ce2aefdc5a
sol <solomon.lutze@gmail.com>
2022-02-12 22:49:27 -0500

    AI ready attack glint WIP

---
+++ Assets/Scripts/Character/DisableAfterAnimation.cs
@@ -0,0 +1,38 @@
+// Add an Animation Event to a GameObject that has an Animator
+using UnityEngine;
+using System.Collections;
+
+public class DisableAfterAnimation : MonoBehaviour
+{
+  AnimationClip clip;
+  public void Start()
+  {
+    StartCoroutine(WaitThenDisable());
+    // existing components on the GameObject
+    // AnimationClip clip;
+    // Animator anim;
+
+    // // new event created
+    // AnimationEvent evt;
+    // evt = new AnimationEvent();
+
+    // // put some parameters on the AnimationEvent
+    // //  - call the function called PrintEvent()
+    // //  - the animation on this object lasts 2 seconds
+    // //    and the new animation created here is
+    // //    set up to happen 1.3s into the animation
+    // evt.intParameter = 12345;
+    // evt.time = 1.3f;
+    // evt.functionName = "PrintEvent";
+    // clip.
+    // // get the animation clip and add the AnimationEvent
+    // anim = GetComponent<Animator>();
+    // clip = anim.runtimeAnimatorController.animationClips[0];
+    // clip.AddEvent(evt);
+  }
+  IEnumerator WaitThenDisable()
+  {
+    yield return new WaitForSeconds(clip.averageDuration);
+    gameObject.SetActive(false);
+  }
+}
